9f475e985caee3b921c527be215a004068ff8276
AdamH18 <aholsinger@WSSD.local>
2016-09-28 08:52:28 -0400

    Test

---
+++ google.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+	<title>A Website</title>
+</head>
+<body>
+<?php
+echo Hi!
+?>
+
+<div id="google">
+	<h1 style="margin: 0px 0px 10px 0px">Google</h1>
+</div>
+
+<input type="text" size="50"><br><br>
+
+<a class="button" href="index.php">Submit</a>
+
+</body>
+</html>
